82f66742eb296842aa2167fec840113f890f9cea
Gaurav <gauravdeep8660@gmail.com>
2023-09-11 20:44:28 +0530

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+const port = 5000
+
+app.use(express.static(__dirname+"src"))
+
+app.get('/',(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,"src/index.html"))
+})
+app.all("*",(req,res)=>{
+    res.status(404).send("not found")
+})
+
+app.listen(port,()=>{
+    console.log(`listening to ${port}`)
+})
