686d7c1e0a4d97badad508530e4395a719d49741
Gaurav <gauravdeep8660@gmail.com>
2023-09-12 23:21:06 +0530

    init

--- src/card.js
+++ src/card.js
@@ -1,16 +1,41 @@
-let allCardContainer = document.querySelector(".allCardContainer")
-let addNoteBtn =  document.querySelector(".addNoteBtn")
+const allCardContainer = document.querySelector(".allCardContainer")
 
+//creates card on click   
+const addNoteBtn =  document.querySelector(".addNoteBtn")
 addNoteBtn.addEventListener('click',()=>{
-    let card = document.createElement('div')
-    card.setAttribute('class','card mb-3')
-    card.innerHTML =`<div class="card-body">
-                        <h5 class="card-title">Card Note Heading</h5>
-                        <p class="card-text">ipsum dolor sit amet consectetur adipisicing elit. Sit nesciunt et facere neque pariatur blanditiis accusamus perspiciatis placeat rem ipsam nostrum deserunt dignissimos consequatur nobis, eos veritatis unde. Aperiam, nobis!</p>
-                        <a href="#" class="btn btn-success">View Full</a>
-                        <a href="#" class="btn btn-success">Delete</a>
-                    </div>`
-    
-    allCardContainer.appendChild(card) 
+    const cardHeading =  document.querySelector("#card_heading")
+    const cardNote =  document.querySelector("#card_note")  
+    let heading = cardHeading.value
+    let note = cardNote.value
+    if(heading!=""&&note!=""){
+        createCard(heading,note)
+        cardNote.value = ""
+        cardHeading.value =""
+    }
 })
+// const noteObj = []
+function createCard(heading,note){
+    let card = document.createElement('div')
+        card.setAttribute('class','card mb-3')
+        card.innerHTML =`<div class="card-body">
+                            <h5 class="card-title">${heading}</h5>
+                            <p class="card-text">${note}</p>
+                            <a href="#" class="btn btn-success">View Full</a>
+                            <a href="#" class="btn btn-success deleteCardBtn">Delete</a>
+                        </div>`
+        
+        allCardContainer.appendChild(card) 
+}
+//deletes a card on 
+const deleteCardBtn =  document.querySelectorAll(".deleteCardBtn")
+allCardContainer.addEventListener('click',(e)=>{
+       if( e.target.getAttribute("class").includes("deleteCardBt")){
+        e.target.parentNode.parentNode.remove()
+       }
+    
+})
+
+// deleteCardBtn.addEventListener('click',(e)=>{
+//     console.log(e.target.parentNode.parentNode)   
+// })
 
